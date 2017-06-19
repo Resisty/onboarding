015671a7b783ddcb1ed8cc13e807859fb7310543
Brian Auron <Brian.Auron@jivesoftware.com>
2017-06-19 16:37:06 -0700

    Abstracting tests similarly to examples

--- run_tests.py
+++ run_tests.py
@@ -5,16 +5,36 @@ Run unit tests
 Example:
     python run_tests.py
 '''
+import argparse
+import importlib
 import sys
 import unittest
-import importlib
 
 PACKAGE = 'onboardtests'
+PACKAGES = ['onboardtests']
+
+def main():
+    ''' Main entry function
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--package',
+                        type=str,
+                        choices=PACKAGES,
+                        default=PACKAGE)
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
+
+def run(args):
+    ''' Run the package tests from arguments
+    '''
+    exit_code = 0
+    pkg = importlib.import_module(args.package)
+    for modname in pkg.__all__:
+        module = importlib.import_module('%s.%s' % (args.package, modname))
+        suite = module.suite()
+        exit_code |= not unittest.TextTestRunner().run(suite).wasSuccessful()
+    sys.exit(exit_code)
+
 if __name__ == '__main__':
-    EXIT_CODE = 0
-    PKG = importlib.import_module(PACKAGE)
-    for modname in PKG.__all__:
-        module = importlib.import_module('%s.%s' % (PACKAGE, modname))
-        SUITE = module.suite()
-        EXIT_CODE |= not unittest.TextTestRunner().run(SUITE).wasSuccessful()
-    sys.exit(EXIT_CODE)
+    main()
